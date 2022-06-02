f4b224b5dffed3d6988e9c95d0cbdd339e20ed20
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-06-02 21:07:41 +0300

    moved abs magnitude calculation to eponymous function

--- modeling/star.py
+++ modeling/star.py
@@ -2,9 +2,13 @@ from math import log10, pi
 
 from astropy.constants.codata2018 import G, sigma_sb
 from astropy.constants.iau2015 import L_sun
-
 from data.constants import *
+
 import modeling.transit as transit
+
+
+def abs_magnitude(L: float):
+	return abs_magnitude_sun - log10(L / L_sun.value) / 0.4
 
 
 class Star:
@@ -15,15 +19,13 @@ class Star:
 		
 		self.square: float = pi * radius ** 2
 		self.square_full: float = 4 * self.square
+		
+		self.abs_magnitude = abs_magnitude(self.L)
 	
 	@property
 	def L(self) -> float:
 		return self.square_full * sigma_sb.value * self.temperature ** 4
 	
-	@property
-	def abs_magnitude(self) -> float:
-		return abs_magnitude_sun - log10(self.L / L_sun.value) / 0.4
-
 
 class StarSystem:
 	def __init__(self, star1: Star, star2: Star, period: float, eccentricity: float, i: float):
