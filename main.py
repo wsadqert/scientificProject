a641b30be69236cc9da8a025a9c37c266938e436
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-06-03 11:18:07 +0300

    tm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import configparser
 from typing import Final
+
 from modeling.star import Star, StarSystem
 
 data = configparser.ConfigParser()
@@ -13,5 +14,9 @@ star2: Star = Star(*[float(data['Star 2'][i]) for i in parameters_star])
 
 system: StarSystem = StarSystem(star1, star2, *[float(data['System'][i]) for i in parameters_system])
 
+for fi in range(360):
+	A: float = system.Period
+	pass
+	
 print(system.calculate_transit(star1, star2), system.abs_magnitude)
 print(system.calculate_touch(star1, star2, star1.radius - 0.5 * star2.radius))
